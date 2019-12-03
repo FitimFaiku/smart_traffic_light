bc70deaa79213aa964764d2d3aa35e7f0105569e
Fitim Faiku <fitim.faiku@wrwks.at>
2019-12-03 10:03:34 +0100

    Showing hours through setTime function and interrupts

--- lcd_module/display.c
+++ lcd_module/display.c
@@ -5,8 +5,10 @@ Test the DOGM-Display over SPI.
 */
 #define F_CPU 16000000 
 #include "dogm_lcd.h"
-#include <avr/io.h>
+#include <avr/io.h> // SFR definitions, bit positions etc.
 #include <util/delay.h>
+#include <avr/interrupt.h> // Interrupts importieren
+#include <avr/sleep.h>
 
 
 void setTime(uint8_t hour, uint8_t minutes, uint8_t seconds){
@@ -15,6 +17,28 @@ void setTime(uint8_t hour, uint8_t minutes, uint8_t seconds){
     lcdWriteChar(minutes);
     lcdWriteChar(':');
     lcdWriteChar(seconds);
+    
+}
+volatile
+ISR(TIMER0_OVF_vect){ // timer 0 overflow interrupt service routine (1 ms)
+    static uint8_t cnt_ms=0, cnt_s=0, cnt_min=0, cnt_hour=0;; // gloabl lifetime, local visibillity Counter for miliseconds
+    TCNT0 = 6; // counter auf 6 --> jede 256-6= 250 ticks --> 1 ms
+    if(cnt_ms++>=100){
+        cnt_s++;
+        if(cnt_s>=60){
+            cnt_min++;
+            if(cnt_min>=60){
+                cnt_hour++;
+                if(cnt_hour>=24){
+                    cnt_hour=0;
+                }
+                cnt_min=0;
+            }
+            cnt_s=0;
+        }
+        cnt_ms=0;
+        setTime(cnt_hour,cnt_min,cnt_s);
+    }
     
 }
 
@@ -27,11 +51,15 @@ void LCD_and_Spi_Init(){
 }
 
 int main(void) {
-	int i = 5;
+    TCCR0B = 3; // prescaler 64 -> 4us tick time, 250 ticks -- 1 ms
+    TIMSK0 = 1 ; // enablen der overflow interrupts
+    TCNT0 = 6; // counter auf 6 --> jede 256-6= 250 ticks --> 1 ms
+    sei(); //enable interrupts(globally)
+    
     LCD_and_Spi_Init();
 
 	_delay_ms(2000);
-    setTime(12,32,20);   // TBD: implement lcdWriteString ...
+       // TBD: implement lcdWriteString ...
 
 	/** _delay_ms(2000);
 	lcdSetCursor(1,1);
@@ -69,8 +97,10 @@ int main(void) {
 
 	lcdOnOff(LCD_ON);
 */
-
+    //Sleep mode setzen(configurieren)
+    set_sleep_mode(SLEEP_MODE_IDLE);
 	while (1){
+        sleep_mode(); //save power
 		//for(uint8_t i=0; i<5; i++) {
 		//	lcdWriteString("Hello World");
 		//}
