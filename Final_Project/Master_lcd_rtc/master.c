b3ab1fdf1ea41a411ffa802235b7457fd42bce33
Fitim Faiku <fitim.faiku@wrwks.at>
2020-01-06 15:41:55 +0100

    Addaptions to Master

--- Final_Project/Master_lcd_rtc/master.c
+++ Final_Project/Master_lcd_rtc/master.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h> //for itoa function
 #include <stdio.h> // For sprintf
+#include <avr/sleep.h>
+
 #define PORT_DIRECTION DDRB
 #define PORT_VALUE PORTB
 
@@ -21,24 +23,32 @@
 #define SS_SELECT PORT_VALUE &= ~(1 << SS);
 
 void check_current_state_and_update_if_needed(void);
-static char current_status = '0';
+static char current_state = '0';
 
 void SPI_MasterInit(void) {
     // Set MOSI and SCK output, all others input
-    PORT_DIRECTION |= (1 << MOSI) | (1 << SCK);
+    PORT_DIRECTION |= (1 << MOSI) | (1 << SCK) | (1<<SS);
     // Enable SPI, Master, set clock rate fck/16
     SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0) | (1 << SPR1);
 }
 
 
-void SPI_MasterTransmit(char cData) {
+uint8_t SPI_MasterTransmit(char cData) {
     // Start transmission
     SPDR = cData;
-
     // Wait for transmission complete
     while (!(SPSR & (1 << SPIF)));
+    
+    return SPDR;
 }
 
+char SPI_MasterReceive(void) {
+    SPDR = 0x00;
+    /* Wait for reception complete */
+    while (!(SPSR & (1 << SPIF)));
+    /* Return Data Register */
+    return SPDR;
+}
 void uart_transmit(char character) {
     while (!(UCSR0A & (1 << 5)));
     UDR0 = (uint8_t) character;
@@ -73,14 +83,6 @@ void uart_init(uint32_t baudrate) {
     // UCSR0B |= (1<<RXCIE0); we do not use interrupts by now..
 }
 
-char SPI_MasterReceive(void) {
-    SPDR = 0x00;
-    /* Wait for reception complete */
-    while (!(SPSR & (1 << SPIF)));
-    /* Return Data Register */
-    return SPDR;
-}
-
 ISR(TIMER0_OVF_vect){ // timer 0 overflow interrupt service routine (1 ms)
 	// TODO get those values from the master module and set them initialy and afterwards display them!!!
     static uint8_t menueopencounter=0, cnt_ms=0,cnt_ms_ten=0, cnt_s=0, cnt_min=26, cnt_hour=11; // gloabl lifetime, local visibillity Counter for miliseconds
@@ -113,14 +115,21 @@ ISR(TIMER0_OVF_vect){ // timer 0 overflow interrupt service routine (1 ms)
 }
 
 void check_current_state_and_update_if_needed(void) {
-    switch (current_status){
-		static uint8_t counter_send_night_mode_status = 0;
+    //TODO check if slave gives some message here and react on that.
+    char slave_message = '3';
+    switch (current_state){
+        static uint8_t counter_send_night_mode_status = 0;
+        static uint8_t datafromslave = 0;
+        static char last_state = '0';
 		case '0':
 		if(counter_send_night_mode_status++>=5){
 			SS_SELECT
 			_delay_ms(100);
-			SPI_MasterTransmit('A');
-			uart_transmit_string("2 gesendet");
+			datafromslave = SPI_MasterTransmit('A');
+            uart_transmit_string("2 gesendet \n \r");
+            uart_transmit_string("Und bekommen");
+            uart_transmit(datafromslave+48)
+            uart_transmit_string("\n \r");
 			SS_UNSELECT
 			counter_send_night_mode_status=0;
 		}
@@ -131,15 +140,16 @@ void check_current_state_and_update_if_needed(void) {
         //Day Mode(will not be  send) -->  Init function set traffic Light green and Walker Red 
         break;
         case '2':
-		uart_transmit_string("2 senden\n\r");
-		//Night Mode --> Send to slaves code 2 
-		SS_SELECT
-		_delay_ms(100);
-		SPI_MasterTransmit('B');
-		uart_transmit_string("2 Gesendet\n\r");
-		SS_UNSELECT
-		current_status='0';
-		
+        if(current_state != last_state){
+            uart_transmit_string("2 senden\n\r");
+            //Night Mode --> Send to slaves code 2 
+            SS_SELECT
+            _delay_ms(100);
+            SPI_MasterTransmit('2');
+            uart_transmit_string("2 Gesendet\n\r");
+            last_state = '2';
+            SS_UNSELECT
+        }
         break;
         //Switch to Red Traffic Light and Green Walkers Traffic Light
         case '3':
@@ -158,10 +168,27 @@ void check_current_state_and_update_if_needed(void) {
         break;
         default:
         uart_transmit_string("default");
-        uart_transmit(current_status);
+        uart_transmit(current_state);
         break;
         
 
+    }
+}
+
+void check_current_hour_and_update_current_state(){
+    uint8_t current_hour = get_current_hour();
+    // 21-6:00 Nachtmodus TODO right times here
+    if(current_hour<=21){ 
+        //Night mode
+        uart_transmit_string("Nachtmodus");
+        //uart_transmit(current_hour+48);
+        current_state = '2';
+        uart_transmit_string("Nachtmodus, status: ");
+        uart_transmit(current_state);
+    } else {
+        // Day mode
+        uart_transmit_string("Tagesmodus");
+        current_state = '1';
     }
 }
 
@@ -173,61 +200,26 @@ int main() {
 
     DDRB |= SS;
     SPI_MasterInit();
-
     // INIT for the real time clock
     init_DS13xx();
 
     // Initialize the SPI interface for the LCD display
     // Initialize the LCD display
     //LCD_and_Spi_Init();
+    
+    check_current_hour_and_update_current_state();
 
-
-    /**
-    currentHour = get_Current_Hour();
-
-    char bufferHour[3];
-    itoa(currentHour,bufferHour,10);
-    uart_transmit_string("Current Time: ");
-    uart_transmit_string(bufferHour);
-    */
+    //Enable interrupts
     TCCR0B = 3; // prescaler 64 -> 4us tick time, 250 ticks -- 1 ms
     TIMSK0 = 1 ; // enablen der overflow interrupts
     TCNT0 = 6; // counter auf 6 --> jede 256-6= 250 ticks --> 1 ms
     sei(); //enable interrupts(globally)
     
-    uint8_t current_hour = get_current_hour();
-		// 21-6:00 Nachtmodus
-	if(current_hour<=21){
-		//Night mode
-		uart_transmit_string("Nachtmodus");
-		//uart_transmit(current_hour+48);
-		current_status = '2';
-		uart_transmit_string("Nachtmodus, status: ");
-		uart_transmit(current_status);
-	} else {
-		// Day mode
-		uart_transmit_string("Tagesmodus");
-		current_status = '1';
-	}
+    //Sleep mode setzen(configurieren)
+    set_sleep_mode(SLEEP_MODE_IDLE);
     
-
-    while (1) {
-        
-       /* SS_SELECT
-        _delay_ms(100);
-        SPI_MasterTransmit('A');
-        SS_UNSELECT
-        uart_transmit_string("255 Gesendet\n\r");
-        _delay_ms(100);
-
-
-        SS_SELECT
-        _delay_ms(100);
-        SPI_MasterTransmit('Z');
-        uart_transmit_string("0 Gesendet\n\r");
-        SS_UNSELECT
-        _delay_ms(100);
-        */
+    while(1){
+        sleep_mode(); //save power
     }
 }
 
